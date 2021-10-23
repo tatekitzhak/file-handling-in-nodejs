c853963ceb946507e54651353cce9cd5dd1b01f6
tatekitzhak <tatekosi@gmail.com>
2021-10-23 16:21:05 +0300

    modified files

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const path = require('path');
+const fs = require('fs');
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  const directoryPath = path.join(__dirname, 'static/file.txt');
+
+  fs.readFile(directoryPath, 'utf8', (err, data)=>{
+    if(err){
+      return console.log("unable to scan directory:"+err);
+    }
+    console.log('data:'+data)
+    // files.forEach((file,index)=>{
+    //   console.log('file:'+file[0])
+    // })
+  })
+  console.log("path:",directoryPath)
   res.send('Hello Node!!')
 })
 
