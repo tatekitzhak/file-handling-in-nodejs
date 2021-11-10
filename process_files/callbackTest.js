b4c3eb26c398604c7594204dfb0fd010784f2bb2
tatekitzhak <tatekosi@gmail.com>
2021-11-10 14:56:02 +0200

    Modify an existing files

--- process_files/callbackTest.js
+++ process_files/callbackTest.js
@@ -2,24 +2,29 @@
 function callbackFunc() {
 
 
-    function download(url,callback) {
+    function download(url, success, failure) {
         setTimeout(() => {
             // script to download the picture here
             console.log(`Downloading ${url} ...`);
-            callback(url);
+            // over simplification
+            let error = url.length === 0 || !url; 
+            // call the failure or success callback
+            error ? failure(url) :  success(url);
             
         }, 3000);
     };
 
-    function process(picture) {
-        console.log(`Processing ${picture}`);
-    };
-
+   
     let url = 'https';
 
-    download(url,process);
-
-
+    download('p',
+        function(picture) {
+            console.log(`Processing the picture ${picture}`);
+        },
+        function(){
+            console.log(`Handling error...`);
+        }
+    );
 
 }
 
