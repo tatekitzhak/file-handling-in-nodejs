5994e6374978330b14a3f7ca350947ae5a5987ad
tatekitzhak <tatekosi@gmail.com>
2021-11-12 17:26:32 +0200

    Modify an existing file

--- process_files/callbackTest3.js
+++ process_files/callbackTest3.js
@@ -1,3 +1,5 @@
+//https://ruben.verborgh.org/blog/2012/12/31/asynchronous-error-handling-in-javascript/
+
 function postLetter(letter, address) {
     if (canSendTo(address)) {
       letter.sendTo(address);
@@ -33,3 +35,59 @@ try {
     else
       onFailure("Cannot reach address " + address);
   }
+
+  postLetter(myLetter, myAddress,
+    function (trackingCode) {
+      console.log("Letter sent with code " + trackingCode);
+    },
+    function (errorMessage) {
+      console.error("Letter not sent: " + errorMessage);
+    });
+
+    function postLetter(letter, address, callback) {
+        if (canSendTo(address))
+          letter.sendTo(address, function () {
+            callback(null, letter.getTrackingCode());
+          });
+        else
+          callback("Cannot reach address " + address);
+      }
+      //If the first argument is non-null, an error has occurred. This can be seen in the caller:
+      
+      postLetter(myLetter, myAddress,
+        function (errorMessage, trackingCode) {
+          if (errorMessage)
+            return console.error("Letter not sent: " + errorMessage);
+          console.log("Letter sent with code " + trackingCode);
+        });
+        var trackingCodePromise = postLetter(myLetter, myAddress);
+trackingCodePromise.done(function (trackingCode) {
+  console.log("Letter sent with code " + trackingCode);
+});
+trackingCodePromise.fail(function (errorMessage) {
+  console.log("Letter not sent: " + errorMessage);
+});
+
+
+foo(arg, (err, val) => {
+    if (err) {
+         console.log(err);
+    } else {
+         val += 1;
+         bar(val, (err1, val1) => {
+              if (err) {
+                   console.log(err1);
+              } else {
+                   val1 += 2;
+                   baz(val1, (err2, result) => {
+                        if (err2) {
+                             console.log(err2);
+                        } else {
+                             result += 3;
+                             console.log(result); // 6
+                        }
+                   });
+              }
+         });
+    }
+});
