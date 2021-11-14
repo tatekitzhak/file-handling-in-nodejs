a44e1090b9a9713a46eef1a30c59cd6cf20b8430
tatekitzhak <tatekosi@gmail.com>
2021-11-14 21:54:12 +0200

    Modify an existing file, i.e: Create a capacity hold a files content array list

--- process_files/readAllFilesDir.js
+++ process_files/readAllFilesDir.js
@@ -1,24 +1,41 @@
 var fs = require('fs');
+const readFile = require('./readFileLines');
 
 function readFiles(dirname, onFileContent, onError) {
-    
+    let filesContentArrayList = [];
   fs.readdir(dirname, function(err, filenames) {
     if (err){
       onError(`Occuerd a error:${err}`);
       return;
     }
-
+    
     filenames.forEach(function(filename, i) {
+        const result = readFile.readFileLineByLine(dirname + filename);
+
+        result.then(function(res){
+            filesContentArrayList.push(res);
+            console.log('File name:', i,res);
+            
+        }).catch((err) => {
+            console.log('err:', err);
+          })
+          .finally(() => {
+            console.log('Files array:', filesContentArrayList)
+          });
+       /*  
         console.log('File name:', i, filename);
-      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        fs.readFile(dirname + filename, 'utf-8', function(err, content) {
         if (err) {
             onError(`Occuerd a error:${err}`);
-          return;
+            return;
         }
         console.log('File Content:',i, content);
-      });
+        }); 
+        */
     });
+    
   });
+  
 }
 
 /*  Here's the storing part: 
