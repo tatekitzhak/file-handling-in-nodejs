b6eb0b07e6d7b8147f1ece267792365ee5d6122f
tatekitzhak <tatekosi@gmail.com>
2022-01-03 15:34:58 +0200

    Modify an existing files: fixed port issue and update db_2.js.

--- index.js
+++ index.js
@@ -2,55 +2,55 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
 
 const readAllFilesDir = require('./process-files/readAllFilesDir');
 const jsonReaderHandle = require('./process-files/readJSONFile');
 const database = require('./mysql-connection/connect');
 const db = require('./mysql-connection/db');
-const dirJsonFile = path.join(__dirname,'static/output.json');
+const dirJsonFile = path.join(__dirname, 'static/output.json');
 
-app.get('/', function(req, res){
-  const dirPath = path.join(__dirname,'static/topics/');
-  
+app.get('/', function (req, res) {
+  const dirPath = path.join(__dirname, 'static/topics/');
+
   function errorHandling(err) {
     console.log('Reading directory issues:', err)
-    
+
   }
 
   function receiveContent(content) {
     console.log('Content:', content);
     var jsonObj = JSON.stringify(content);
-    
-    fs.writeFile(dirJsonFile, jsonObj, 'utf8', function(err) {
+
+    fs.writeFile(dirJsonFile, jsonObj, 'utf8', function (err) {
       if (err) {
         console.log("An error occured while writing JSON Object to File.");
         return console.log(err);
       }
-  
+
       console.log(`Write a JSON file and has been saved at :${dirJsonFile}`);
     });
   }
 
-  readAllFilesDir.readFilesHandle(dirPath,receiveContent, errorHandling);
-  
+  readAllFilesDir.readFilesHandle(dirPath, receiveContent, errorHandling);
+
   res.send('http://localhost:3000 ...');
- 
+
 });
 
 
-app.get('/json_reader', function(req, res){
-  jsonReaderHandle.jsonReader(dirJsonFile, function(content){
-    
-      content.forEach(function(obj ,index){
-        
-        for (const nameFile in obj) {
-          console.log(`----------${index}-----------`)
-          for(let i=0; i < obj[nameFile].length; i++)
-            console.log(`Content ${index} : [${nameFile}] = ${obj[nameFile][i]}`);
-        }
-      });
-  },function(err){
+app.get('/json_reader', function (req, res) {
+  jsonReaderHandle.jsonReader(dirJsonFile, function (content) {
+
+    content.forEach(function (obj, index) {
+
+      for (const nameFile in obj) {
+        console.log(`----------${index}-----------`)
+        for (let i = 0; i < obj[nameFile].length; i++)
+          console.log(`Content ${index} : [${nameFile}] = ${obj[nameFile][i]}`);
+      }
+    });
+  }, function (err) {
     console.log('Error Handling:', err)
   });
 
@@ -58,13 +58,13 @@ app.get('/json_reader', function(req, res){
 });
 /*  
   MySQL Database
-*/ 
-app.get("/mysql_database", function(req, res){
-  
-  database.database_connection(function(successfulData){
-    
+*/
+app.get("/mysql_database", function (req, res) {
+
+  database.database_connection(function (successfulData) {
+
     console.log(`Database Connection: ${successfulData}`)
-  }, function(err){
+  }, function (err) {
     console.log(`Error Establishing a Database Connection: ${err}`)
   });
 
@@ -74,26 +74,25 @@ app.get("/mysql_database", function(req, res){
 
 
 const Cat = require('./mysql-connection/db_test');
-const area = require('./mysql-connection/db_2');
-app.get('/db',function(req, res) {
+const db_2 = require('./mysql-connection/db_2');
 
-  const cat = new Cat();
-//console.log('Cat:',cat.makeSound());
-  /* db.dbCnnection(function(successfulData){
-    
-    console.log(`DB Connection: ${successfulData}`)
-  }, function(err){
-    console.log(`Error Establishing a DB Connection: ${err}`)
+app.get('/db', function (req, res) {
+
+  db_2.database_connection(function (successful) {
+
+    console.log(`Database Connection: ${successful}`);
+    console.log('successful:',successful)
+  }, function (err) {
+    console.log(`Error Establishing a Database Connection: ${err}`)
   });
-   */
-  //db.db_connection;
-  res.json(area.area(3));
+
+  res.json(db_2.database_connection());
 });
 
 
-app.listen(port, function(err){
+app.listen(port, function (err) {
   if (err) {
-		throw err;
-	}
+    throw err;
+  }
   console.log(`Node Endpoints working at: http://localhost:${port}`)
 });
--- mysql-connection/db_2.js
+++ mysql-connection/db_2.js
@@ -1,14 +1,37 @@
-const PI = 3.14159265359;
+const mysql = require('mysql');
+const host = '127.0.0.1';
 
- function area(radius) {
+
+const db_config = {
+    host    : host,
+    user    : 'ran',
+    password: 'ran',
+    database: 'db1',
+    debug: false,
+  };
+var db_connection;
+
+function database_connection(successfulHadle, errorHadle) {
+    
+    db_connection = mysql.createConnection(db_config); //Connecting to MySQL database server
+
+    db_connection.connect(function(err) {
+      if (err){
+        return errorHadle && errorHadle(`The execution line of code function : database_connection() ${err}`);
+    }
+      return successfulHadle && successfulHadle({1:`The database connection is successful to the MySQL server`});
+       
+    }); 
+    return db_config;
+}
+
+
+function area(radius) {
   return {mult: radius*2}
 }
 
- function circumference(radius) {
-  return 2 * radius * PI;
-}
 
 module.exports = {
     area,
-    circumference
+    database_connection
   }
