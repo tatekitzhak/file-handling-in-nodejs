907bda781b5faa9a28896ef09ed1074f1740dd3b
tatekitzhak <tatekosi@gmail.com>
2022-01-25 15:35:20 +0200

    Updated a feature of regex.js file script.

--- regex.js
+++ regex.js
@@ -13,3 +13,11 @@ let pattern_begin_of_str = /^[\s|\W|_]*/g
 
 let res = str.replace(pattern_begin_of_str, '')
 console.log(res);
+
+/**
+ * Detect string which contains only spaces
+ */
+let str = "    ";
+if (!str.replace(/\s/g, '').length) {
+  console.log('string only contains whitespace (ie. spaces, tabs or line breaks)');
+}
